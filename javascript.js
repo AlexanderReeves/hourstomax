6452c071f8d5c99bb208de87422908b04eff1c63
AlexanderReeves <reevesdrums@gmail.com>
2024-06-05 12:20:23 +1000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -176,7 +176,7 @@ function RefreshPlayer(){
 	//This code runs on page load, or submit hsername button.
 
 
-	$.getJSON("https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player="+user, function(result){
+	$.getJSON("http://cors.io/?https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player="+user, function(result){
 	    $.each(result, function(i, field){
 	//json player data is returned one line at a time
 	//the key aka outer value is a number
